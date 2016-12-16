352fb4a4924c459e22d3eaaaaf5e6801dea335c6
hughlau <hughlaunet@gmail.com>
2016-12-16 16:42:14 +0800

    es_client

--- hlms-net/src/main/java/com/hl/es/client/JavaCli.java
+++ hlms-net/src/main/java/com/hl/es/client/JavaCli.java
@@ -1,5 +1,20 @@
 package com.hl.es.client;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
 public class JavaCli {
 
+    public Client buildClient(String myClusterName,String ip,String port) throws NumberFormatException, UnknownHostException {
+        Settings settings = Settings.builder()
+                        .put("cluster.name", myClusterName).build();
+        TransportClient client = TransportClient.builder().settings(settings).build()
+        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(ip), Integer.parseInt(port)));
+        return client;
+    }
 }
